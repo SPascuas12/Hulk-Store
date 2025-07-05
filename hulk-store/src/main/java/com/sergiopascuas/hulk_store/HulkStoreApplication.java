771a055a58c0c50c58417ca343f74aff7f4f51a7
SergioPascuas <sapascuas-2021a@corhuila.edu.co>
2025-07-05 08:24:14 -0500

    chore(security): se desactiva CSRF y se habilita frameOptions para consola H2

--- hulk-store/src/main/java/com/sergiopascuas/hulk_store/HulkStoreApplication.java
+++ hulk-store/src/main/java/com/sergiopascuas/hulk_store/HulkStoreApplication.java
@@ -1,12 +1,13 @@
-package com.hulk_store.hulk_store;
+package com.sergiopascuas.hulk_store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sergiopascuas.hulk_store")
 public class HulkStoreApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HulkStoreApplication.class, args);
 	}
 
