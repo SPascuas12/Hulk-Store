d2e2903c356010c441f68321719d56fdaac87f47
SPascuas12 <sergiopascuas123@gmail.com>
2025-07-15 11:14:49 -0500

    Agrego carpeta Hulk-Store_Backend con backend del proyecto

--- hulk-store/src/main/java/com/sergiopascuas/hulk_store/repository/ProductoRepository.java
+++ hulk-store/src/main/java/com/sergiopascuas/hulk_store/repository/ProductoRepository.java
@@ -2,8 +2,6 @@ package com.sergiopascuas.hulk_store.repository;
 
 import com.sergiopascuas.hulk_store.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
